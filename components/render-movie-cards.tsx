f049eb60019372f27953b55f3164828250d2e0fd
Javier Aahmes Reansyah <javierreansyah@gmail.com>
2026-07-28 21:11:27 +0700

    fix build error

--- components/render-movie-cards.tsx
+++ components/render-movie-cards.tsx
@@ -1,23 +1,6 @@
 import React from "react";
 import MovieCard from "./movie-card";
-
-interface Movie {
-  adult: boolean;
-  backdrop_path: string;
-  id: number;
-  title: string;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  poster_path: string;
-  media_type: string;
-  genre_ids: number[];
-  popularity: number;
-  release_date: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
+import { Movie } from "@/types";
 
 interface RenderCardsProps {
   movies: Movie[];
